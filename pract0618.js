7297ac7217a7cd54b85943f7acbcefcb75bb6d1a
Martin Dezsi <dezsimartin@gmail.com>
2019-06-18 16:33:20 +0200

    up

---
+++ pract0618.js
@@ -0,0 +1,61 @@
+/* const generateArray = (a) => {
+  let tomb = [1];
+  for (let i = 0; i < a; i++) {
+    tomb[i] = Math.floor(Math.random() * 10) + 1;
+  }
+
+  return tomb;
+};
+*/
+
+const smallestDifference = (arr1, arr2) => {
+  let diff2 = 20;
+  let temp1;
+  let temp2;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if ((diff2 > arr1[i] - arr2[j] && arr1[i] - arr2[j] >= 0) || (diff2 > arr2[j] - arr1[i] && arr2[j] - arr1[i] >= 0)) {
+        diff2 = arr1[i] - arr2[j];
+        temp1 = arr1[i];
+        temp2 = arr2[j];
+      }
+    }
+  }
+
+  return [temp1, temp2];
+};
+
+const stepSum = (arr) => {
+  if (arr.length === 1) {
+    console.log(arr);
+    return arr;
+  } else {
+    let tomb = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i > 0) {
+        tomb.push(arr[i] + arr[i - 1]);
+      }
+    }
+
+    tomb = stepSum(tomb);
+    console.log(arr);
+  }
+};
+
+const permutations = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+
+  }
+  return arr;
+};
+
+/* let arr1 = [10, 3, 2, 1];
+console.log(arr1);
+let arr2 = [6, 8, 9, 10];
+console.log(arr2);
+console.log(smallestDifference(arr1, arr2));
+*/
+
+let array = [1, 2, 3];
+// console.log(permutations(array));
+stepSum(array);
